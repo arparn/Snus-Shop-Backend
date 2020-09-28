bf700363ba19ba1326410de06921c6aa1f591d7b
Nikita <nibirj@ttu.ee>
2020-09-28 19:29:28 +0300

    #15 searching by price and strength

--- src/main/java/ee/taltech/webpage/controller/ItemController.java
+++ src/main/java/ee/taltech/webpage/controller/ItemController.java
@@ -19,11 +19,24 @@ public class ItemController {
     public List<Item> getItems(@RequestParam(value = "name", required = false) String name){
         return itemsService.getAll();
     }
-    //todo searching
+
     @GetMapping("{name}")
     public List<Item> getItemByName(@RequestParam String name) {
         return itemsService.getByName(name);
     }
 
+    @GetMapping("{rating}")
+    public List<Item> getItemByRating() {
+        return itemsService.getByRating();
+    }
 
+    @GetMapping("{strength}")
+    public List<Item> getItemByStrength() {
+        return itemsService.getByStrength();
+    }
+
+    @GetMapping("{price}")
+    public List<Item> getItemByPrice() {
+        return itemsService.getByPrice();
+    }
 }
