6c88ec75140ff3267744960d8b87b668b9b56dbe
arthur <arthurp200148@gmail.com>
2020-10-02 14:34:50 +0300

    Fix backend and frontend connection. ref #20

--- src/main/java/ee/taltech/webpage/ItemApplicationUnit.java
+++ src/main/java/ee/taltech/webpage/ItemApplicationUnit.java
@@ -17,11 +17,11 @@ public class ItemApplicationUnit implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         List<Item> items = List.of(
-                new Item((long) 1, "Oden's", "/images/odens.png", 5.60, "Good Snus", 4.0, 5),
-                new Item((long) 2, "Thunder", "/images/thunder.png", 7.20, "Good Snus", 4.2, 4),
-                new Item((long) 3, "Siberia", "/images/siberia.png", 4.80, "Good Snus", 3.8, 5),
-                new Item((long) 4, "KNOX", "/images/knox.png", 6.00, "Good Snus", 4.6, 3),
-                new Item((long) 5, "Skruf", "/images/skruf.png", 4.99, "Good Snus", 3.7, 4)
+                new Item( (long) 1, "Oden's", "/assets/images/odens.png", 5.60, "Snus", 4.0, 5),
+                new Item( (long) 2, "Thunder", "/assets/images/thunder.png", 7.20, "Snus", 4.2, 4),
+                new Item( (long) 3, "Siberia", "/assets/images/siberia.png", 4.80, "Snus", 3.8, 5),
+                new Item( (long) 4, "KNOX", "/assets/images/knox.png", 6.00, "Snus", 4.6, 3),
+                new Item( (long) 5, "Skruf", "/assets/images/skruf.png", 4.99, "Snus", 3.7, 4)
         );
 
         itemsRepository.saveAll(items);
--- src/main/java/ee/taltech/webpage/comments/Comment.java
+++ src/main/java/ee/taltech/webpage/comments/Comment.java
@@ -7,8 +7,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-
-
 @Getter
 @Setter
 @Entity
