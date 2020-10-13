91143ab2a5fa793bf17475ee65bc4107817e4c7f
ilboit <ilboit@ttu.ee>
2020-10-13 16:09:40 +0300

    #33 Change item creation

--- src/main/java/ee/taltech/webpage/model/Item.java
+++ src/main/java/ee/taltech/webpage/model/Item.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Item {
 
@@ -23,28 +22,25 @@ public class Item {
     private String url;
     private Double price;
     private String description;
-    private Double ratingsCount;
-    private Double ratingsSum;
-    private Double rating;
+    private Double ratingsCount = 0.0;
+    private Double ratingsSum = 0.0;
+    private Double rating = 0.0;
     private Integer strength;
     @ManyToMany
     private List<Comment> comments = new LinkedList<>();
 
-    public Item(Long id, String name, String url, Double price, String description, Double rating, Integer strength) {
+    public Item(Long id, String name, String url, Double price, String description, Integer strength) {
         this.id = id;
         this.name = name;
         this.url = url;
         this.price = price;
         this.description = description;
-        this.rating = rating;
         this.strength = strength;
-        ratingsSum = 0.0;
-        ratingsCount = 0.0;
     }
 
     public void addGrade(Integer grade){
         ratingsSum += Double.valueOf(grade);
-        ratingsCount += 1.0;
+        ratingsCount ++;
         rating = (double) ratingsSum / ratingsCount;
     }
 
