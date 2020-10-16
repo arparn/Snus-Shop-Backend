c5d167f1684791777bdae89122ba5d16bd9f3b7e
ilboit <ilboit@ttu.ee>
2020-10-16 15:14:25 +0300

    #40 answer 1 question

---
+++ src/main/java/ee/taltech/webpage/a_theory/question1/Compilation.java
@@ -0,0 +1,23 @@
+package ee.taltech.webpage.a_theory.question1;
+
+public class Compilation {
+
+    //todo
+    // Sometimes things get complicated, sometimes things get compiled.
+
+    //todo p1
+    // In your words (do not use wiki definitions)
+    // What is compilation?
+    // Answer: Compilation is way to transform symbols and words to 0 and 1,
+    // so the computer can understand human directions.
+
+    //todo p2
+    // Name 2 benefits of compilation?
+    // 1 It make faster to write code.
+    // 2 The amount of directions the human should remember is less,
+    // as a huge amount of 0 and 1 is under one quick command.
+
+    //todo p3
+    // Name 1 disadvantage of compilation?
+    // 1 Compilation takes time, so the amount of work for computer is bigger.
+}
---
+++ src/main/java/ee/taltech/webpage/a_theory/question6/chocolate/Cake.java
@@ -0,0 +1,12 @@
+package ee.taltech.webpage.a_theory.question6.chocolate;
+
+import java.util.List;
+
+public class Cake {
+
+    private Long id;
+    private String size;
+    private String sweetness;
+    private List<String> ingredients;
+    private List<String> toppings;
+}
