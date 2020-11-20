aa22be624da350969f21da23e7db5fab50b73695
Ilja Boitsuk <ilboit@ttu.ee>
2020-11-20 23:54:05 +0200

    #50 Answer on question 11 D part

--- src/main/java/ee/taltech/webpage/b_theory/question7/CI.java
+++ src/main/java/ee/taltech/webpage/b_theory/question7/CI.java
@@ -4,28 +4,28 @@ public class CI {
 
     //todo A
     // What is integration?
-    // Answer:
+    // Answer: Merging working copies into a common main development branch.
 
     //todo B
     // What makes integration continuous?
-    // Answer:
+    // Answer: Frequent merging, up to several times a day.
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick [x] which ones stand for continuous integration
-    // [] compile
+    // [x] compile
     // [] test
     // [] deploy
 
     //todo D
     // Name and explain 1 benefit of CI
-    // 1
+    // 1 quick error elimination - problems of integrations will found quickly and elimination is quick, so the pay for their elimination is not so big.
 
     //todo E
     // Name and explain 1 drawback of CI
-    // 1
+    // 1 There are a need for additional computing resources for continuous integration.
 
     //todo F
     // In the class we are using gitlab-ci. Name 2 alternatives to it
-    // 1
-    // 2
+    // 1 Azure DevOps
+    // 2 TeamCity
 }
--- src/main/java/ee/taltech/webpage/b_theory/question8/CD.java
+++ src/main/java/ee/taltech/webpage/b_theory/question8/CD.java
@@ -4,28 +4,28 @@ public class CD {
 
     //todo A
     // What is delivery?
-    // Answer:
+    // Answer: is an extension of continuous integration. after the build stage it  automatically deploys all code changes.
 
     //todo B
     // What makes delivery continuous?
-    // Answer:
+    // Answer: an automated release process that allows to deploy your application any time.
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick which ones stand for continuous delivery
     // [] compile
     // [] test
-    // [] deploy
+    // [x] deploy
 
     //todo D
     // Name and explain 1 benefit of CD
-    // 1
+    // 1 The complexity of deploying software has been taken away, so you no need to spend a lot of time on release preparation.
 
     //todo E
     // Name and explain 1 drawback of CD
-    // 1
+    // 1 A need in strong CI and tests should cover enough of your code.
 
     //todo F
     // Which one is more important to start with CI or CD? Why?
-    // CI/CD:
-    // Because:
+    // CI/CD: СI
+    // Because: As CD can't work without CI
 }
