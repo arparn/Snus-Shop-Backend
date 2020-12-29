8884f30f757fec68a63eedb586df5d15dba5e327
ARTHUR-ALEKSANDER PÄRNOJA <arthurp200148@gmail.com>
2020-12-29 13:22:38 +0200

    C theory

---
+++ src/main/java/ee/taltech/webpage/c_theory/question12/Security.java
@@ -0,0 +1,22 @@
+package ee.taltech.webpage.c_theory.question12;
+
+public class Security {
+
+    //todo A
+    // What is authentication?
+    // Answer:
+    // Authentication is the process of recognizing the identity of a user. It is a mechanism for associating
+    // an incoming request with a set of credentials. The provided credentials are compared with the data in a file
+    // in the authorized user information database on the local operating system or on the authentication server.
+
+    //todo B
+    // What is authorization?
+    // Answer:
+
+    //todo C
+    // For web (HTTP backend) Spring Security is configured as a ... ? Explain it.
+    // Think or read how Spring Security checks are injection into your application.
+    // Configured as (one word):
+    // Description:
+
+}
