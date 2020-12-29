50802a3a439e7f8e83e99e68397533a3ba354026
Ilja Boitsuk <ilboit@ttu.ee>
2020-12-29 15:09:47 +0200

    #61 theory c fix

--- src/main/java/ee/taltech/webpage/c_theory/question12/Security.java
+++ src/main/java/ee/taltech/webpage/c_theory/question12/Security.java
@@ -6,11 +6,13 @@ public class Security {
     // What is authentication?
     // Answer:
     // Authentication is a mechanism to check the identity of a user by comparing the password entered by
-    // user (for the specified login) with the password stored in the user login database.
+    // user (for the specified login) with the password stored in the user login database. This is needed to the
+    // application to verify user who he is to separate him from others.
 
     //todo B
     // What is authorization?
     // Answer: Authorization is a mechanism of giving certain rights to group of certain people to make certain actions.
+    // This is to make a concept of roles/permissions to divide rights.
 
     //todo C
     // For web (HTTP backend) Spring Security is configured as a ... ? Explain it.
