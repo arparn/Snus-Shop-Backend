b2277a3c73e6e83bc8a94789c9083720e2e8d67c
ARTHUR-ALEKSANDER PÄRNOJA <arthurp200148@gmail.com>
2020-12-29 14:34:25 +0200

    C theory

--- src/main/java/ee/taltech/webpage/c_theory/question13/MaterialDesign.java
+++ src/main/java/ee/taltech/webpage/c_theory/question13/MaterialDesign.java
@@ -8,6 +8,7 @@ public class MaterialDesign {
 
     //todo B
     // Name and describe 2 principles of material design (there are more than 2)
-    // 1
-    // 2
+    // 1 Bold, graphic, and intentional: Everything that we design should be deliberate and big.
+    // 2 Motion provides meaning: Motion can be used to help move the user and create meaning, in particular,
+    // movement is especially useful in providing feedback to the user.
 }
